afd693a287d3571707814239f327a6e3edff9dad
Joseph Cameron <jf.cameron@gmail.com>
2021-01-18 16:19:51 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+// © 2021 Joseph Cameron - All Rights Reserved
+
+#include <cstdlib>
+
+int main(int count, char **args)
+{
+    return EXIT_SUCCESS;
+}
---
+++ test/example_test.cpp
@@ -0,0 +1,14 @@
+// © 2021 Joseph Cameron - All Rights Reserved
+
+#include <jfc/catch.hpp>
+#include <jfc/types.h>
+
+//using namespace jfc;
+
+TEST_CASE( "jfc::example test", "[jfc::example]" )
+{
+    SECTION("example does something correctly")
+    {
+        REQUIRE(true);
+    }
+}
